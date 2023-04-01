81db83802f366792f060cae8de8a3a3b2d2710ba
Ryu-Okamoto <u514203b@ecs.osaka-u.ac.jp>
2023-04-01 20:11:20 +0900

    scene transitions

--- src/App.js
+++ src/App.js
@@ -4,76 +4,74 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 import { socket } from "./components/socket";
 
 function App() {
-  const [user, setUser] = useState("");
-  const users = useRef([]);
-  const positions = useRef([]);
-  const { unityProvider, sendMessage , addEventListener, removeEventListener } = useUnityContext({
+  const [userName, setUserName] = useState("");
+  const playerNames = useRef([]);
+  const playerPositions = useRef([]);
+  const { unityProvider, isLoaded, sendMessage, addEventListener} = useUnityContext({
     loaderUrl: "Build/test.loader.js",
     dataUrl: "Build/test.data",
     frameworkUrl: "Build/test.framework.js",
     codeUrl: "Build/test.wasm",
   });
 
-  const handleInformUsername = useCallback((username) => {
-    setUser(username);
-    changeToWaiting();
-    socket.emit("c2sRequestJoin", { "user": username });
-  });
-  
-  const handleInformReady = useCallback(() => {
-    socket.emit("c2sOK", { "user": user });
-  });
-
-  const handleInformPullInfo = useCallback((directionX, directionY, rotation) => {
-    socket.emit("c2sPull", 
-      { 
-        "user": user , 
-        "pullInfo": {
-          "directionX": directionX,
-          "directionY": directionY,
-          "rotation": "null" 
-        }
-      }
-    );
-  }, [user]);
-
-  const handleInformPosition = useCallback((isLast, username, positionX, positionY, positionZ) => {
-    positions.current.push(
-      { 
-        "user": username , 
-        "positionX": positionX,
-        "positionY": positionY,
-        "positionZ": positionZ
-      }
-      );
-    if (isLast === 1) {
-      socket.emit("c2sInformPositions", { "user": user, "positions": positions.current });
-      positions.current.splice(0);
+  // 「Start」ボタン押下時の処理の登録
+  useEffect(() => {
+    if (isLoaded) {
+      addEventListener("InformUsername", async function (userName) {
+        setUserName(userName);
+        sendMessage("TitleManager", "ChangeToWaiting");
+        socket.emit("c2sRequestJoin", { "user": userName });
+      });
     }
-  }, [user]); 
+  }, [isLoaded]);
 
-  // message to TitleManager
-  function postNotice(content) {
-    sendMessage("TitleManager", "PostNotice", content);
-  }
-  function changeToWaiting() {
-    sendMessage("TitleManager", "ChangeToWaiting");
-  }
+  // 「Ready」ボタン押下時の処理の登録
+  useEffect(() => {
+    if (userName) {
+      addEventListener("InformReady", async function () {
+        console.log(userName);
+        socket.emit("c2sOK", { "user": userName });
+      });
+    }
+  }, [userName]);
 
-  // message to WaitingManager
-  function setContent(jsonString) {
-    sendMessage("WaitingManager", "SetContent", jsonString);
-  }
-  function changeToGame() {
-    sendMessage("WaitingManager", "ChangeToGame");
-  }
+  // ユーザが消しゴムを引っ張ったときの処理の登録
+  useEffect(() => {
+    addEventListener("InformPullInfo", async function (directionX, directionY, rotation) {
+      socket.emit("c2sPull",
+        {
+          "user": userName,
+          "pullInfo": {
+            "directionX": directionX,
+            "directionY": directionY,
+            "rotation": "null"
+          }
+        }
+      );
+    })
+  });
 
-  // message to GameManager
-  function setup(jsonString) {
-    sendMessage("GameManager", "Setup", jsonString);
-  }
+  // 位置の計算が終わり、その結果が送られてきたときの処理の登録
+  useEffect(() => {
+    addEventListener("InformPosition", async function (isLast, playerName, positionX, positionY, positionZ) {
+      playerPositions.current.push(
+        { 
+          "user": playerName, 
+          "positionX": positionX,
+          "positionY": positionY,
+          "positionZ": positionZ
+        }
+      );
+      if (isLast === 1) {
+        socket.emit("c2sInformPositions", { "user": userName, "positions": playerPositions.current });
+        playerPositions.current.splice(0);
+      }
+    });
+  });
+
+
   function enablePull() {
-    sendMessage("GameManager", "EnablePull", user);
+    sendMessage("GameManager", "EnablePull", userName);
   }
   function reflectPullInfo(jsonString) {
     sendMessage("GameManager", "ReflectPullInfo", jsonString);
@@ -82,61 +80,56 @@ function App() {
     sendMessage("GameManager", "SynchronizePositions", jsonString);
   }
   
+  // サーバからの情報が送られてきたときの処理の登録
   useEffect(() => {
-    socket.on("connect", (data) => {});
+    if (isLoaded && userName) {
 
-    socket.on("s2cInformUsers", (data) => {
-      users.current = data["users"];
-      
-      // check
-      let infos = data["users"].map((name) => {return {"name": name, "ready": 1};});
-      setContent("{ \"infos\": " + JSON.stringify(infos) + "}");
-    });
+      // 参加者の入退室時
+      socket.on("s2cInformUsers", (data) => {
+        playerNames.current = data["users"];
+        const infos = data["users"].map((name) => {return {"name": name, "ready": 1};});
+        sendMessage("WaitingManager", "SetContent", "{ \"infos\": " + JSON.stringify(infos) + "}");
+      });
 
-    socket.on("s2cStart", (json) => {
-      const firstUser = json["firstUser"];
-      users.current = json["users"];
-      changeToGame();
-      setup("{ \"users\": " + JSON.stringify(users.current) + " }");
-      if (firstUser === user)
-        enablePull(firstUser);
-    });
+      // ゲーム開始時
+      socket.on("s2cStart", (json) => {
+        const firstPlayer = json["firstUser"];
+        playerNames.current = json["users"];
+        sendMessage("WaitingManager", "ChangeToGame");
+        sendMessage("GameManager", "Setup", "{ \"users\": " + JSON.stringify(playerNames.current) + " }");
 
-    socket.on("s2cSharePull", (json) => {
-      if (user !== json["user"])
-        reflectPullInfo(JSON.stringify(json));
-    });
+        // TODO : missing method exception "GameMain.EnablePull"
+        if (firstPlayer === userName)
+          sendMessage("GameManager", "EnablePull", userName);
+      });
 
-    socket.on("s2cAveragePositions", (data) => {
-      synchronizePositions(JSON.stringify({ "positions": data["positions"] }));
-      console.log(data["nextUser"])
-      if (user === data["nextUser"]) 
-        enablePull(user);
-    });
-    
-    socket.on("s2cInformResult", (data) => {
-      const result = data["result"];
-      console.log(result);
-    });
+      // 引っ張った情報の共有時
+      socket.on("s2cSharePull", (json) => {
+        if (userName !== json["user"])
+          reflectPullInfo(JSON.stringify(json));
+      });
 
-    socket.on("disconnect", (data) => {
-      console.log(data);
-      // TODO: 結果の出力
-    });
-  }, []);
+      // 位置情報の同期時
+      socket.on("s2cAveragePositions", (data) => {
+        const nextPuller = data["nextUser"];
+        synchronizePositions(JSON.stringify({ "positions": data["positions"] }));
 
-  useEffect(() => {
-    addEventListener("InformUsername", handleInformUsername);
-    addEventListener("InformReady", handleInformReady);
-    addEventListener("InformPullInfo", handleInformPullInfo);
-    addEventListener("InformPosition", handleInformPosition);
-    return () => {
-      removeEventListener("InformUsername", handleInformUsername);
-      removeEventListener("InformReady", handleInformReady);
-      removeEventListener("InformPullInfo", handleInformPullInfo);
-      removeEventListener("InformPosition", handleInformPosition);
-    };
-   }, [addEventListener, removeEventListener, handleInformPullInfo, handleInformPosition]);
+        // TODO : missing method exception "GameMain.EnablePull"
+        if (nextPuller === userName) 
+          sendMessage("GameManager", "EnablePull", userName);
+      });
+
+      // ゲーム終了時
+      socket.on("s2cInformResult", (data) => {
+        const result = data["result"];
+        console.log(result);
+      });
+    }
+    else {
+      socket.on("connect", () => { console.log("connected"); });
+      socket.on("disconnect", () => { console.log("disconnected"); });
+    }
+  }, [isLoaded, userName]);
 
   return (
     <div 

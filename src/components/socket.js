8afbe36a9abaece5b8faeb4cfe79d28f1c87b9c8
09B20006 <09B20006@localhost>
2023-05-02 14:23:34 +0900

    changed globalIP

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("localhost:5001/", {
+export const socket = io("112.71.101.132:5001/", {
     transports: ["websocket"],
     cors: {
       origin: "http://localhost:3000/",
